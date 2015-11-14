9b12cd926db5d3e07ff1a456025140e83ffc0731
Dragomitch <orohimesama@gmail.com>
2015-11-14 21:48:58 +0100

    PUSH002: Complétion de houses_submit_estimate
    Complétion de houses_submit_estimate

--- SQL/src/houses_submit_estimate.sql
+++ SQL/src/houses_submit_estimate.sql
@@ -28,14 +28,18 @@ DECLARE
   arg_house_id ALIAS FOR $7;
   new_estimate_request_id INTEGER;
 BEGIN
-  IF EXISTS( --If the estimate_request is expired, we raise a exception;
-    SELECT *
-    FROM marche_halibaba.estimate_request er
-    WHERE er.estimate_request_id= arg_estimate_request_id  
-      AND er.deadline< NOW()
-    )THEN 
-      RAISE EXCEPTION 'Cette demande de devis est expirée.';
+  IF arg_is_secret = TRUE
+    THEN RAISE EXCEPTION data_exception;
+  END IF;
+  
+  IF arg_is_hiding = TRUE
+    THEN RAISE EXCEPTION data_exception;
+  END IF;
 
+  IF arg_is_cancelled= TRUE
+    THEN RAISE EXCEPTION data_exception;
+  END IF;
+  
   IF EXISTS(
     SELECT *
     FROM marche_halibaba.houses h
@@ -43,6 +47,16 @@ BEGIN
       AND h.penalty_expiration > NOW()
     )THEN 
       RAISE EXCEPTION 'Vous êtes interdit de devis pour le moment.';
+  END IF;
+
+    IF EXISTS( --If the estimate_request is expired, we raise a exception;
+    SELECT *
+    FROM marche_halibaba.estimate_request er
+    WHERE er.estimate_request_id= arg_estimate_request_id  
+      AND er.deadline< NOW()
+    )THEN 
+      RAISE EXCEPTION 'Cette demande de devis est expirée.';
+  END IF;
 
   INSERT INTO marche_halibaba.estimate(description, price, is_cancelled, is_secret, is_hiding, submission_date, estimate_request_id, house_id)
     VALUES (arg_description, arg_price, arg_is_cancelled, arg_is_secret, arg_is_hiding, NOW(), arg_estimate_request_id, arg_house_id)
